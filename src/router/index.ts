3d520fcbbb598d276432123ad69a04d117988aea
SteveKingFord <332312568@qq.com>
2021-06-27 21:24:03 +0800

    feat: computed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,19 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-27 11:14:16
- * @LastEditTime: 2021-06-27 20:39:44
+ * @LastEditTime: 2021-06-27 21:05:10
  */
 import { createRouter, createWebHashHistory } from 'vue-router';
 import refComp from '@/composition/ref.vue';
 import renativeComp from '@/composition/reactive.vue';
+import computedComp from '@/composition/computed.vue';
 const Home = { template: '<div>Home</div>' };
-const About = { template: '<div>About</div>' };
 
 const routes = [
   { path: '/', component: Home },
   { path: '/ref', component: refComp },
   { path: '/reactive', component: renativeComp },
-  { path: '/about', component: About },
+  { path: '/computed', component: computedComp },
 ];
 
 export const router = createRouter({
