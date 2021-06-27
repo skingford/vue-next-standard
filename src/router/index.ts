656a6007ee3b5842591ebb1bb163bbf14a3ba14d
SteveKingFord <332312568@qq.com>
2021-06-27 17:29:21 +0800

    chore: 配置别名

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,16 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-27 11:14:16
- * @LastEditTime: 2021-06-27 12:44:03
+ * @LastEditTime: 2021-06-27 12:50:28
  */
 import { createRouter, createWebHashHistory } from 'vue-router';
-// import refComp from '@/composition/ref.vue';
+import refComp from '@/composition/ref.vue';
 const Home = { template: '<div>Home</div>' };
 const About = { template: '<div>About</div>' };
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/ref', component: refComp },
   { path: '/about', component: About },
 ];
 
