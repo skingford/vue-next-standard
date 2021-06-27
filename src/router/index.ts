61a865d80a994e3774e69e292abf84999a7ea96e
SteveKingFord <332312568@qq.com>
2021-06-27 22:10:25 +0800

    feat: watch

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,16 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-27 11:14:16
- * @LastEditTime: 2021-06-27 21:05:10
+ * @LastEditTime: 2021-06-27 22:06:46
  */
 import { createRouter, createWebHashHistory } from 'vue-router';
 import refComp from '@/composition/ref.vue';
 import renativeComp from '@/composition/reactive.vue';
 import computedComp from '@/composition/computed.vue';
-const Home = { template: '<div>Home</div>' };
+import watchComp from '@/composition/watch.vue';
 
 const routes = [
-  { path: '/', component: Home },
+  { path: '/', component: watchComp },
   { path: '/ref', component: refComp },
   { path: '/reactive', component: renativeComp },
   { path: '/computed', component: computedComp },
