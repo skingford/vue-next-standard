13c230eb1ea55d78645c5f5b422969a2ced34fab
kingford <stevekingford@gmail.com>
2021-07-16 14:30:24 +0800

    upd: 修改样式规范

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,27 +1,29 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-14 22:23:09
- * @LastEditTime: 2021-07-16 12:46:07
+ * @LastEditTime: 2021-07-16 14:28:51
  */
 module.exports = {
   extends: 'stylelint-config-standard',
   rules: {
+    'at-rule-no-unknown': null,
     'at-rule-name-case': 'lower',
     'at-rule-name-space-after': 'always-single-line',
     'color-no-invalid-hex': true,
-    'rule-empty-line-before': null,
     'color-hex-length': 'long',
     'color-hex-case': 'lower',
-    'unit-whitelist': ['em', 'rem', '%', 's', 'px', 'deg', 'vh', 'vw'],
-    'unit-case': 'lower',
-    'value-keyword-case': 'lower',
     'declaration-colon-newline-after': null,
-    'at-rule-no-unknown': null,
+    'font-family-no-duplicate-names': true,
     'font-family-no-missing-generic-family-keyword': [
       true,
       {
         ignoreFontFamilies: ['PingFangSC'],
       },
     ],
+    'length-zero-no-unit': true,
+    'rule-empty-line-before': null,
+    'unit-whitelist': ['em', 'rem', '%', 's', 'px', 'deg', 'vh', 'vw'],
+    'unit-case': 'lower',
+    'value-keyword-case': 'lower',
   },
 };
