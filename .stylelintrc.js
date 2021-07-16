61d4a137d7445f8c0be1176f8b2e2b15b4da7ae0
kingford <stevekingford@gmail.com>
2021-07-16 12:48:27 +0800

    chore: 引入字体

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,17 +1,27 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-14 22:23:09
- * @LastEditTime: 2021-07-16 00:50:07
+ * @LastEditTime: 2021-07-16 12:46:07
  */
 module.exports = {
   extends: 'stylelint-config-standard',
   rules: {
+    'at-rule-name-case': 'lower',
+    'at-rule-name-space-after': 'always-single-line',
     'color-no-invalid-hex': true,
     'rule-empty-line-before': null,
     'color-hex-length': 'long',
     'color-hex-case': 'lower',
     'unit-whitelist': ['em', 'rem', '%', 's', 'px', 'deg', 'vh', 'vw'],
+    'unit-case': 'lower',
+    'value-keyword-case': 'lower',
     'declaration-colon-newline-after': null,
     'at-rule-no-unknown': null,
+    'font-family-no-missing-generic-family-keyword': [
+      true,
+      {
+        ignoreFontFamilies: ['PingFangSC'],
+      },
+    ],
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-13 01:43:14
- * @LastEditTime: 2021-07-16 11:15:59
+ * @LastEditTime: 2021-07-16 12:02:15
  */
 import { createApp } from 'vue';
 import { router } from './router';
@@ -11,7 +11,6 @@ import storage from '@utils/storage';
 
 // global styles
 import 'normalize.css/normalize.css';
-// import 'ant-design-vue/dist/antd.css';
 import './styles/index.scss';
 
 import useAnt from './hooks/useAnt';
