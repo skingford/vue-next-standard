0f2b71ec9e34e0b5eecb09251b5bc3ef206f3d93
SteveKingFord <332312568@qq.com>
2021-07-24 23:54:25 +0800

    feat: 添加路由白名单

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,30 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-27 11:14:16
- * @LastEditTime: 2021-07-24 21:09:28
+ * @LastEditTime: 2021-07-24 23:41:42
  */
-import { basicRoutes } from './routes';
-
 import { App } from 'vue';
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router';
-// import refComp from '@/compositions/ref.vue';
-// import renativeComp from '@/compositions/reactive.vue';
-// import computedComp from '@/compositions/computed.vue';
-// import watchComp from '@/compositions/watch.vue';
-// import LayoutComp from '../layouts/index.vue';
 
-// const routes: Array<RouteRecordRaw> = [
-//   {
-//     path: '/',
-//     component: LayoutComp,
-//     children: [
-//       {
-//         path: '',
-//         component: watchComp,
-//         name: 'Home',
-//         meta: {
-//           title: '主页',
-//           keepAlive: true,
-//         },
-//       },
-//       { path: 'my', component: computedComp },
-//     ],
-//   },
-//   { path: '/login', component: () => import('../views/login') },
-//   { path: '/ref', component: refComp },
-//   { path: '/reactive', component: renativeComp },
-//   { path: '/computed', component: computedComp },
-// ];
+import { REDIRECT_NAME } from './constant';
+import { basicRoutes, LoginRoute } from './routes';
+const WHITE_NAME_LIST = [LoginRoute.name, REDIRECT_NAME];
 
-const router = createRouter({
+export const router = createRouter({
   history: createWebHashHistory(),
   routes: basicRoutes as Array<RouteRecordRaw>,
   strict: true,
   scrollBehavior: () => ({ left: 0, top: 0 }),
 });
+
+export function resetRouter() {
+  router.getRoutes().forEach((route) => {
+    const { name } = route;
+    if (name && !WHITE_NAME_LIST.includes(name as string)) {
+      router.hasRoute(name) && router.removeRoute(name);
+    }
+  });
+}
 
 export function setupRouter(app: App<Element>) {
   app.use(router);
