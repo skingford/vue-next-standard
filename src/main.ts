7cf8d8e3f26b5a1467e05645bc08dfb9036279cb
kingford <stevekingford@gmail.com>
2021-07-24 10:27:47 +0800

    chore: 样式目录变动

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 /*
  * @Author: kingford
  * @Date: 2021-06-13 01:43:14
- * @LastEditTime: 2021-07-24 10:16:06
+ * @LastEditTime: 2021-07-24 10:26:39
  */
+
+// global styles
+import 'normalize.css/normalize.css';
+import '@/styles/tailwind.css';
+import '@/styles/index.scss';
+
 import { createApp } from 'vue';
 import { setupRouter } from './router';
 import {
@@ -11,12 +17,6 @@ import {
 } from '@utils/globalProperties';
 
 import App from './App.vue';
-// import App from './App';
-
-// global styles
-import 'normalize.css/normalize.css';
-import './styles/index.scss';
-import './styles/index.css';
 
 function bootstrap() {
   const app = createApp(App);
